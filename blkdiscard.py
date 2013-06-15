7a12f4f30b874d3eceafca6fc3768fffe3d84f7d
Ryan Smith-Roberts <rmsr@lab.net>
2013-06-14 21:47:21 -0700

    add ability to discard in chunks

--- blkdiscard.py
+++ blkdiscard.py
@@ -1,7 +1,19 @@
 #! /usr/bin/env python
 
-"""
-Zero out a device under Linux by discarding the contents
+USAGE = """
+Usage: blkdiscard [ -c SIZE ] DEVICE
+
+Discards the entire contents of DEVICE using the BLKDISCARD ioctl, as if it
+were ovewritten with zeroes. Supported devices may include SSDs, flash cards,
+and other flash-based storage devices, as well as network storage or volume
+management systems with "thin provisioning" features.
+
+Flags:
+    -c SIZE     Discard the device in chunks of SIZE megabytes, rather than
+                all at once. Use this to prevent ioctl timeout errors if
+                the discard operation is slow, such as with some SATA drives.
+
+WARNING: THIS WILL PERMANENTLY ERASE THE ENTIRE CONTENTS OF YOUR DEVICE. YHBW.
 """
 
 import sys
@@ -10,20 +22,9 @@ import fcntl
 import struct
 import getopt
 
-BLKGETSIZE64 = 0x80081272
-BLKDISCARD = 0x1277
-BLKDISCARDZEROES = 0x127c
-
-USAGE = """
-Usage: blkdiscard [-q] device
-
-Discards the entire contents of DEVICE using the BLKDISCARD ioctl, as if it
-were ovewritten with zeroes. Supported devices may include SSDs, memory cards,
-and other flash storage devices, as well as network storage or volume
-management systems with "thin provisioning" features.
-
-THIS WILL PERMANENTLY DESTROY ALL CONTENTS OF THE DEVICE. YHBW. HTH. HAND.
-"""
+IOCTL_BLKGETSIZE64 = 0x80081272
+IOCTL_BLKDISCARD = 0x1277
+IOCTL_BLKDISCARDZEROES = 0x127c
 
 def usage(msg=None):
     out = sys.stderr if msg else sys.stdout
@@ -33,33 +34,42 @@ def usage(msg=None):
     print >>out, USAGE.strip()
     sys.exit(1 if msg else 0)
 
-def msg(fmt, *args):
-    if not quiet:
-        sys.stdout.write(fmt % args)
-
 def ioctl(fd, req, fmt, *args):
     buf = struct.pack(fmt, *(args or [0]))
     buf = fcntl.ioctl(fd, req, buf)
     return struct.unpack(fmt, buf)[0]
 
+def discard_chunk(fd, offset, size):
+    ioctl(fd, IOCTL_BLKDISCARD, 'LL', offset, size)
+
+def discard(fd, chunksize):
+    size = ioctl(fd, IOCTL_BLKGETSIZE64, 'L')
+    ioctl(fd, IOCTL_BLKDISCARDZEROES, 'I')
+    if not chunksize:
+        discard_chunk(fd, 0, size)
+        return
+    for offset in xrange(0, size, chunksize):
+        discard_chunk(fd, offset, min(chunksize, size, - offset))
+
 def main(args):
-    opts, args = getopt.gnu_getopt(args, 'qh')
-    if ('-h', '') in opts:
-        usage()
-    global quiet
-    quiet = ('-q', '') in opts
-    if not args:
-        usage("Missing block device argument")
-    elif len(args) > 2:
-        usage("Only one device argument allowed")
-
+    chunksize = 0
+    helpopt = ( '-h', '' )
+    opts, args = getopt.gnu_getopt(args, 'hc:')
+    for flag, val in opts:
+        if flag == '-h':
+            usage()
+        elif flag == '-c':
+            try:
+                chunksize = int(val)
+                if chunksize < 1:
+                    raise ValueError
+            except ValueError:
+                usage("Chunk size must be a positive nonzero integer")
+            chunksize *= 1024 ** 2
+    if len(args) != 1:
+        usage("Exactly one device argument required")
     with open(args[0], 'w') as dev:
-        fd = dev.fileno()
-        size = ioctl(fd, BLKGETSIZE64, 'L')
-        ioctl(fd, BLKDISCARDZEROES, 'I')
-        # TODO: discard in segments and display progress
-        ioctl(fd, BLKDISCARD, 'LL', 0, size)
-
+        discard(dev.fileno(), chunksize)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
